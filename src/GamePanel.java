3c7c2d94f9f2a7f6cd62364aa503488ff5b0c535
Tom DiMonte <tdimonte@luc.edu>
2021-10-30 18:23:25 -0500

    Added grid for UNIT_SIZE testing

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -42,9 +42,17 @@ public class GamePanel extends JPanel implements ActionListener{
                 timer.start();
         }
         public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                draw(g);
 
         }
         public void draw(Graphics g) {
+
+                //***SHOW UNIT_SIZE GRID***
+                // for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
+                //         g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+                //         g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+                // }
 
         }
         public void newPoint(){
